1df9ae69060569ce564014fbbb6897c3190b7d33
ClaroTechUK <clarotechuk@gmail.com>
2017-01-05 13:33:55 +0000

    Remove comment

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace Clarotech.Verifier
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
